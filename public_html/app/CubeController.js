94c3db5dd64b8f62224fb940e755dfb44a032e55
FABIO ARIAS <fharias@gmail.com>
2016-01-25 11:37:07 -0500

    Realizar suma de Cubo, solo salidas Console.

--- public_html/app/CubeController.js
+++ public_html/app/CubeController.js
@@ -26,12 +26,12 @@ gravility.controller('CubeController', ['$scope', function ($scope) {
                             i++;
                             if(n >= 1 && n <= 100){
                                 $scope.cube = initCube(n);
-                                console.log($scope.cube);
                                 if(m >= 1 && m <= 1000){
                                     var test = lines.slice(i, i+m);
                                     i=i+m;
                                     test.forEach(function(e){
-                                        if(/^(^UPDATE|update)\s(\d{0,3}\s\d{0,3}\s\d{0,3}\s(-?\d*\.{0,1}\d+))$/.test(e)){
+                                        if(/^(^UPDATE|update)\s(\d{0,3}\s\d{0,3}\s\d{0,3}\s(-?\d*\.{0,1}\d+))$/
+                                                .test(e)){
                                             var update = e.split(' ');
                                             if((parseInt(update[1]) <= n && parseInt(update[1])>=1)
                                                     && (parseInt(update[2]) <= n && parseInt(update[2])>=1)
@@ -40,11 +40,24 @@ gravility.controller('CubeController', ['$scope', function ($scope) {
                                                     && parseInt(update[4])<=Math.pow(10,9))){
                                                 $scope.cube[update[1]-1][update[2]-1][update[3]-1] = update[4];
                                             }
-                                        }else if(/^(^QUERY|query)\s(\d{0,3}\s\d{0,3}\s\d{0,3}\s\d{0,3}\s\d{0,3}\s\d{0,3})$/.test(e)){
-                                            
+                                        }else if(/^(^QUERY|query)\s(\d{0,3}\s\d{0,3}\s\d{0,3}\s\d{0,3}\s\d{0,3}\s\d{0,3})$/
+                                                .test(e)){
+                                            var query = e.split(' ');
+                                            var x1 = parseInt(query[1]);
+                                            var y1 = parseInt(query[2]);
+                                            var z1 = parseInt(query[3]);
+                                            var x2 = parseInt(query[4]);
+                                            var y2 = parseInt(query[5]);
+                                            var z2 = parseInt(query[6]);
+                                            if(x1 >= 1 && x1 <= n && y1 >= 1 
+                                                    && y1 <= n && z1 >=1 && z1 <= n 
+                                                    && x2 >= 1 && x2 <= n 
+                                                    && y2 >= 1 && y2 <= n
+                                                    && z2 >= 1 && z2 <= n){
+                                                console.log(sumCube(x1,x2,y1,y2,z1,z2));
+                                            }
                                         }else{
                                             console.log('Format command not valid');
-                                            throw('Format command not valid');
                                         }
                                     });
                                 }else{
@@ -79,5 +92,17 @@ gravility.controller('CubeController', ['$scope', function ($scope) {
                 }
             }
             return cube;
+        };
+        
+        var sumCube = function(x1,x2,y1,y2,z1,z2){
+            var sum = 0;
+            for(var i=x1-1; i < x2; i++){
+                for(var j=y1-1; j < y2; j++){
+                    for(var k=z1-1; k < z2; k++){
+                        sum += parseInt($scope.cube[i][j][k]);
+                    }
+                }
+            }
+            return sum;
         };
     }]);
